5d557278a0389ae8c0e7f8a58671eb805b9b1c24
akannika <a.kannika80@gmail.com>
2023-10-22 15:16:56 -0700

    Update more detail, add pop-up

--- app.js
+++ app.js
@@ -13,3 +13,51 @@ $(".menu-bar").on("click", function() {
 $(".logo").on("click", function() {
         $(".sidebar").removeClass("active");
 });
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay');
+const main = document.getElementById('main');
+const detail = document.getElementById('detail');
+const sidebar = document.getElementById('sidebar');
+
+openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+                const modal = document.querySelector(button.dataset.modalTarget);
+                openModal(modal)
+        })
+})
+
+main.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+                closeModal(modal)
+        })
+})
+
+sidebar.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+                closeModal(modal)
+        })
+})
+
+closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+                const modal = button.closest('.modal')
+                closeModal(modal)
+        })
+})
+
+function openModal(modal) {
+        if (modal == null) return
+        modal.classList.add('active')
+        overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+        if (modal == null) return
+        modal.classList.remove('active')
+        overlay.classList.remove('active')
+}
+
